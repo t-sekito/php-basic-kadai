d05718749120bb0c567387cc723abc394afd6bc7
t-sekito <mtna_s@ms3.megaegg.ne.jp>
2025-05-12 19:45:06 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_sort</title>
+</head>
+
+<body>
+    <p>
+        <?php
+
+        function sort_2way($array, $order) {
+            if ($order == "true") {
+                sort($array);
+            } else if ($order == "false") {
+                rsort($array);
+            }
+            return $array;
+        }
+    
+        $nums = [15, 4, 18, 23, 10 ];
+
+        // 昇順にソートする（sort_2wayの呼び出し）
+        echo "昇順にソートします。<br>";
+        $sort_nums_asc = sort_2way($nums, "true");
+        foreach ($sort_nums_asc as $num) {
+            echo $num . "<br>";
+        }   
+
+        // 降順にソート（sort_2wayの呼び出し）
+        echo "降順にソートします。<br>";
+        $rsort_nums_desc = sort_2way($nums, "false");
+        foreach ($rsort_nums_desc as $num) {
+            echo $num . "<br>";
+        }
+
+        ?>
+    </p>
+</body>
+
+</html>
